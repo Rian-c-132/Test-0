13afc53fd035d6232583c9f1251b48681532d651
Rian <53636103+Rian-c-132@users.noreply.github.com>
2020-09-27 23:38:23 +0600

    test

--- test 0 bot.py
+++ test 0 bot.py
@@ -1,3 +1,19 @@
 import discord
 from discord.ext import commands
 
+client = commands.Bot(command_prefix = '.')
+token = ""
+@client.event
+async def on_ready():
+	print("Ready!")
+
+@client.command()
+async def ping(ctx):
+	await ctx.send(f"Pong! {client.latency * 1000} ms.")
+
+@client.command()
+async def clear(ctx, amount = 5):
+	await ctx.channel.purge(limit = 5)
+
+client.run(token) # token
+
