a011186398f6c951cbdca2960bf1e5da10ca04d2
Rian <53636103+Rian-c-132@users.noreply.github.com>
2020-09-28 13:37:52 +0600

    test

--- test 0 bot.py
+++ test 0 bot.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix = '.')
-token = ""
+token = "NzU5ODAxNzI0OTE4NjkzOTA5.X3Cyfw.5CHdmELjNbwDhpi6qtXybmQ8Ce0"
 @client.event
 async def on_ready():
 	print("Ready!")
@@ -13,7 +14,23 @@ async def ping(ctx):
 
 @client.command()
 async def clear(ctx, amount = 5):
-	await ctx.channel.purge(limit = 5)
+	await ctx.channel.purge(limit = amount)
+
+@client.command()
+async def roast(ctx, member : discord.Member):
+	responses = [ "If I wanted to suicide, I would climb your ego and jump onto your iq.",
+				"You are as useless as the 'ueue' in 'queue'.",
+				"If I had a face like yours, I'd sue my parents.",
+				"Some day you'll go far and I hope you stay there.",
+				"You are a prime candidate for natural de-selection.",
+				"The trash will get picked up tomorrow, be ready.",
+				"You are not useless because you can still be used as a bad example."]
+	await ctx.send(f"{member.mention} {random.choice(responses)}")
+
+@client.command()
+async def pp(ctx, member : discord.Member):
+	sz = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	await ctx.send(f"8{random.choice(sz)*'='}D")
 
 client.run(token) # token
 
