4676e9effc283585a21cdb8d35870240b6af94eb
Rian <53636103+Rian-c-132@users.noreply.github.com>
2020-09-29 22:27:19 +0600

    scrape memes

--- scrapeifunny.py
+++ scrapeifunny.py
@@ -1,17 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://ifunny.co"
 
-source = requests.get(url)
-plain = source.text
-soup = BeautifulSoup(plain, "html.parser")
 urls = []
-def get_urls():
-	for link in soup.find_all("img"):
-		img_link = link.get('data-src')
-		if img_link != None :
-			urls.append(img_link)
+def get_urls(site):
+	if site == 'ifunny' :
+		url = "https://www.ifunny.co/"
+	else :
+		url = "https://www.memedroid.com/"
+
+	source = requests.get(url)
+	soup = BeautifulSoup(source.text, "lxml")
+
+	if site == 'ifunny' :
+		for link in soup.find_all("img"):
+			img_link = link.get('data-src')
+			if img_link != None :
+				urls.append(img_link)
+	else :
+		for link in soup.find_all("img", {'class': 'img-responsive'}):
+			img_link = link.get('src')
+			if img_link[:4] == 'http' :
+				urls.append(img_link)
 	return urls
 #get_urls()
 #print(urls)
