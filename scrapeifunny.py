25bc5a8ef5f180e1243a3c39a72720a72c03b8ad
Rian <53636103+Rian-c-132@users.noreply.github.com>
2020-09-29 21:07:57 +0600

    scrapememe

--- scrapeifunny.py
+++ scrapeifunny.py
@@ -9,10 +9,9 @@ soup = BeautifulSoup(plain, "html.parser")
 urls = []
 def get_urls():
 	for link in soup.find_all("img"):
-		#print("check")
 		img_link = link.get('data-src')
 		if img_link != None :
 			urls.append(img_link)
 	return urls
-get_urls()
-print(urls)
+#get_urls()
+#print(urls)
