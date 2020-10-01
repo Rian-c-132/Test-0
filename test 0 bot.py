1d26f7d40ec6f8975b4ab449a42a96d8c6331a79
Rian <53636103+Rian-c-132@users.noreply.github.com>
2020-10-01 12:22:17 +0600

    test

--- test 0 bot.py
+++ test 0 bot.py
@@ -27,23 +27,6 @@ async def meme(ctx, site = "memedroid"):
 	await ctx.send(embed = embed)
 
 
-@client.command()
-async def roast(ctx, member : discord.Member):
-	responses = [ "If I wanted to suicide, I would climb your ego and jump onto your iq.",
-				"You are as useless as the 'ueue' in 'queue'.",
-				"If I had a face like yours, I'd sue my parents.",
-				"Some day you'll go far and I hope you stay there.",
-				"You are a prime candidate for natural de-selection.",
-				"The trash will get picked up tomorrow, be ready.",
-				"You are not useless because you can still be used as a bad example."]
-	await ctx.send(f"{member.mention} {random.choice(responses)}")
-
-@client.command()
-async def pp(ctx, member : discord.Member):
-	sz = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-	msg = discord.Embed(title = f"{member}'s pp\n",
-		description = f"8{random.choice(sz)*'='}D")
-	await ctx.send(embed = msg)
 
 client.run(token) # token
 
